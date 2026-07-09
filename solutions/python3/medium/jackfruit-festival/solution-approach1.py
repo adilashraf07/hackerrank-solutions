8024092fed773ca9d9b3b502b783a9e003268dc9
MUHAMMED ADIL P P <143965631+adilashraf07@users.noreply.github.com>
2026-07-09 14:39:49 +0530

    ✅ JackFruit Festival [python3] — approach1

---
+++ solutions/python3/medium/jackfruit-festival/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Problem     JackFruit Festival
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-09, 02:39 p.m.
+# ──────────────────────────────────────────────────
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n , d = map(int, input().split())
+weights = list(map(int, input().split()))
+
+weights.sort(reverse=True)
+
+print(sum(weights[:d]))
